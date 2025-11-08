90fefe1b506f8cd06d2012540c885f1616c82b8a
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-11-08 18:13:22 -0500

    frontend setup

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (<div className="flex flex-row justify-center">
+    <h1>Lauren and Nidhi's Color Palette Project</h1>
+  </div>
+  );
+}
